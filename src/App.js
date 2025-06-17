b9112f52f328e3a2365452df22be96b1bd5c8c7b
Sudarshan kalgonda hingalje <47637833+Sudarshanhingalje@users.noreply.github.com>
2025-06-18 01:52:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import { useMovies } from "./useMovies";
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-const KEY = "f84fc31d";
+// const KEY = "f84fc31d";
+const KEY = process.env.REACT_APP_OMDB_KEY;
 
 export default function App() {
   const [query, setQuery] = useState("");
